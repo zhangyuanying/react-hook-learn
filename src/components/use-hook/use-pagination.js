05a0284885e47555cbb31bb6c62e987a8207286c
guowentao <guowentao1@360.cn>
2019-12-15 10:09:20 +0800

    fix: use-pagination

--- src/components/use-hook/use-pagination.js
+++ src/components/use-hook/use-pagination.js
@@ -1,18 +1,38 @@
-import { useMemo } from "react";
+import { useState, useMemo } from "react";
 
-function usePagination(config = {}) {
+export const defaultPagination = {
+  pageSize: 10,
+  current: 1
+};
+
+function usePagination(config = defaultPagination) {
+  const [pagination, setPagination] = useState({
+    pageSize: config.pageSize || defaultPagination.pageSize,
+    current: config.page || config.defaultCurrent || defaultPagination.current
+  });
+
   const paginationConfig = useMemo(() => {
     return {
-      pageSize: config.limit || 10,
-      current: config.page || 1,
-      defaultCurrent: 1,
-      showTotal: total => total,
-      total: config.totalCount || 0,
-      onChange: (page, pageSize) => {
-        config.onChange({ limit: pageSize, page });
+      ...defaultPagination,
+      showTotal: total =>
+        `每页 ${pagination.pageSize} 条  第 ${pagination.current}页 共 ${total}`,
+      ...config,
+      pageSize: pagination.pageSize,
+      current: pagination.current,
+      onChange: (current, pageSize) => {
+        if (config.onChange) {
+          config.onChange(current, pageSize);
+        }
+        setPagination({ pageSize, current });
+      },
+      onShowSizeChange: (current, pageSize) => {
+        if (config.onChange) {
+          config.onChange(current, pageSize);
+        }
+        setPagination({ pageSize, current });
       }
     };
-  }, [config]);
+  }, [config, pagination]);
 
   return paginationConfig;
 }
--- src/pages/list.js
+++ src/pages/list.js
@@ -1,41 +1,59 @@
-import React from "react";
-import { Table } from "antd";
+import React, { useEffect, useState } from "react";
+import { Table, Pagination } from "antd";
 import useFetch from "../components/use-hook/use-fetch";
 import { getJokes } from "../api";
 import usePagination from "../components/use-hook/use-pagination";
 
 function List() {
-  const { data = {}, loading, doFetch } = useFetch(getJokes, { page: 1 });
+  const { data = {}, loading, doFetch } = useFetch(getJokes, {
+    page: 1
+  });
 
-  const paginationConfig = usePagination({
-    ...data,
-    onChange: val => {
-      doFetch(val);
+  const pagination = usePagination({
+    total: data.totalCount,
+    // page: 2,
+    // pageSize: 2,
+    // showQuickJumper: true,
+    // showSizeChanger: true,
+    // defaultCurrent: 2,
+    // onShowSizeChange: () => {},
+    // showTotal: total => total,
+    onChange: (page, limit) => {
+      doFetch({ page, limit });
     }
   });
-  console.log("getList", data, paginationConfig);
+  // console.log("getList", data, pagination);
   const columns = [
     { title: "笑话内容", dataIndex: "content" },
     { title: "更新时间", dataIndex: "updateTime" }
   ];
-  // const pagination = {
-  //   pageSize: data.limit || 10,
-  //   current: data.page || 1,
-  //   defaultCurrent: 1,
-  //   showTotal: total => total,
-  //   total: data.totalCount || 0,
-  //   onChange: (page, pageSize) => {
-  //     console.log("page, pageSize", page, pageSize);
-  //   }
-  // };
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
+  const handle = () => {
+    setDisabled(true);
+    setTimeout(() => {
+      setCount(count + 1);
+      setDisabled(false);
+    }, 1000);
+  };
+  console.log("render");
   return (
-    <Table
-      rowKey="content"
-      loading={loading}
-      pagination={paginationConfig}
-      columns={columns}
-      dataSource={data.list}
-    />
+    <>
+      <p>
+        <button onClick={handle} disabled={disabled}>
+          {count}
+        </button>
+      </p>
+      <Table
+        rowKey="content"
+        loading={loading}
+        pagination={pagination}
+        columns={columns}
+        dataSource={data.list}
+      />
+      <Pagination {...pagination} />
+    </>
   );
 }
 
